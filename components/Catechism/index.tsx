f1c8fc2984d421b249c03c56dc3f848ebc3c7bad
Michael Cheng <cheng.c.mike@gmail.com>
2023-08-16 12:18:47 -0500

    create catechism folder

--- components/Catechism/index.tsx
+++ components/Catechism/index.tsx
@@ -1,7 +1,5 @@
 import { StyleSheet, View } from "react-native";
-import { useState } from "react";
-import DocumentTitle from "./DocumentTitle";
-import ReadCatechism from "./ReadCatechism";
+import ReadCatechism from "../ReadCatechism";
 
 export default function Catechism({ catechism }) {
   return (
